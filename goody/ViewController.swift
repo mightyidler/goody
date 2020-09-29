6cb948c9c7acec4d2590a3ee57a78b5383cfae1d
mightyidler <mightyidler@gmail.com>
2020-09-29 23:41:15 +0900

    Initial Commit

---
+++ goody/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  goody
+//
+//  Created by mightyidler on 2020/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
